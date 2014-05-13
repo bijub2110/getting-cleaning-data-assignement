e778d817b37d0b113099f46b862a935dd39e9434
avidys <jm@avidys.com>
2014-05-12 17:02:46 -0700

    Initial commit

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+# Refs
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+# Tasks
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive activity names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+zipFile <- "./data/Dataset.zip"
+unzipDir <- "."
+dataDir <- "UCI HAR Dataset"
+zipFileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(zipFileUrl,"./data/Dataset.zip",method="curl")
+unzip(zipFile, exdir=unzipDir)
+l <- list.files(unzipDir)
+l
+
+#read.csv(l[1])
